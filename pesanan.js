f1f2ed10e9df6ae7abd3e71e786f4380ef3da690
Muhaza <30515291+muhaza@users.noreply.github.com>
2019-11-09 13:02:28 +0800

    outside js

---
+++ pesanan.js
@@ -0,0 +1,14 @@
+document.getElementById("pesanan").innerHTML =
+    `<a class="align-center btn btn-sm btn-secondary display-4">Versi Anda : BETA</a>`;
+
+document.getElementById("footer-tab").innerHTML =
+    `<div class="media-container-row align-center mbr-white">
+<div class="row row-links" id="footer-button">
+    <ul class="foot-menu">
+        <div class="mbr-section-btn">
+            <a class="btn btn-sm btn-secondary display-4" onclick="bacaButton()">BACA</a>
+            <a class="btn btn-sm btn-white-outline display-4" onclick="submitButton()">HANTAR</a>
+        </div>
+    </ul>
+</div>
+</div>`;
